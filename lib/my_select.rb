2ab74f6b5c817db9cde9a555e8db8c906e0a10b2
Sean Conrad <seanconrad123@gmail.com>
2018-06-22 18:19:02 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,9 +2,10 @@ def my_select(array, num)
   counter = 0
   result = []
   while counter < array.size
-      if yield(array[counter]) == true
-        result.push(array[counter])
-      end
+    if yield(array[counter]) == true
+      result.push(array[counter])
+    end
+    counter += 1
   end
 end
 
