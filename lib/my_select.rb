c73ae7d420ffb3ca25eb016be46d0d531f40252a
Sean Conrad <seanconrad123@gmail.com>
2018-06-22 18:20:37 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  counter = 0
+  result = []
+  while counter < array.size
+    if yield(array[counter]) == true
+      result.push(array[counter])
+    end
+    counter += 1
+  end
+  result
 end
